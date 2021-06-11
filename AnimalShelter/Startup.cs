f22327dad17a96bf919c6a202940120e812cf58d
Jesse White <jesse.white6@gmail.com>
2021-06-11 08:33:11 -0700

    :wrench: add markup to startup and program files.

---
+++ AnimalShelter/Startup.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
+
+using AnimalShelter.Models;
+
+namespace AnimalShelter
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<TravelContext>(opt =>
+          opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddControllers();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "Blind Cat Rescue API",
+          Description = "An API for a local Animal Shelter, Blind Cat Rescue. Use these query commands to search through their inventory.",
+          TermsOfService = new Uri("https://example.com/terms"),
+          Contact = new OpenApiContact
+          {
+            Name = "Jesse White",
+            Email = string.Empty,
+            Url = new Uri("https://github.com/JesseDWhite")
+          },
+          License = new OpenApiLicense
+          {
+            Name = "Use under LICX",
+            Url = new Uri("https://example.com/license"),
+          }
+        });
+      });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseSwagger(c =>
+      {
+        c.SerializeAsV2 = true;
+      });
+
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blind Cat Rescue V1");
+        c.RoutePrefix = string.Empty;
+      });
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
